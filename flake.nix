e8b46d64636f629af5a8139db259b0123eefde5d
Valentin Boettcher <hiro@protagon.space>
2022-07-21 10:11:38 +0200

    add lualatex-math

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
           physics mathtools amsmath fontspec booktabs siunitx caption biblatex float
           pgfplots microtype fancyvrb csquotes setspace newunicodechar hyperref
           cleveref multirow bbold unicode-math biblatex-phys xpatch beamerposter
-          type1cm changepage;
+          type1cm changepage lualatex-math;
       };
     in rec {
       packages = {
