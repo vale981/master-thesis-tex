faf039dcb3a7d1a4ce7e10394cb0434884949dc0
Valentin Boettcher <hiro@protagon.space>
2022-08-15 21:41:53 +0200

    add talk

---
+++ talk/figs/analytic_comp/sync.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+rsync -av ../../../masterarb/python/energy_flow_proper/11_new_ho_comparison/figures/ .
---
+++ talk/figs/one_bath_syst/sync.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env bash
+rsync -av ../../../masterarb/python/energy_flow_proper/07_one_bath_systematics/figures/ .
