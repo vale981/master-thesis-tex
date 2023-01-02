dab42466e1503cf84e3ef6df3655dcd04018978d
Valentin Boettcher <hiro@protagon.space>
2023-01-02 13:30:09 -0500

    begin short talk

--- flake.nix
+++ flake.nix
@@ -8,49 +8,50 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     with flake-utils.lib; eachSystem allSystems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.combine {
-        inherit (pkgs.texlive) scheme-medium latexmk koma-script babel-english
-          physics mathtools amsmath fontspec booktabs siunitx caption biblatex float
-          pgfplots microtype fancyvrb csquotes setspace newunicodechar hyperref
-          cleveref multirow bbold unicode-math biblatex-phys xpatch beamerposter
-          type1cm changepage lualatex-math footmisc wrapfig2 curve2e pict2e wrapfig
-          appendixnumberbeamer sidecap appendix orcidlink ncctools bigfoot crop xcolor;
-      };
-    in rec {
-      packages = {
-        watch = pkgs.writeShellScriptBin "watch-latex" ''
-                 ${tex}/bin/latexmk
-                 while ${pkgs.inotify-tools}/bin/inotifywait --include ".*\.(sty|tex|pgf|pdf)" -e modify -r .; do
-                   ${tex}/bin/latexmk
-                 done
-              '';
-        document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "masters-thesis";
-          src = self;
-          buildInputs = [ pkgs.coreutils tex pkgs.biber];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            mkdir -p output/src
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-               OSFONTDIR=${pkgs.gyre-fonts}/share/fonts:${pkgs.liberation_ttf}/share/fonts:${pkgs.lato}/share/fonts/lato:${pkgs.raleway}/share/fonts:${pkgs.lmodern}/share/fonts \
-              latexmk ./index.tex
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        tex = pkgs.texlive.combine {
+          inherit (pkgs.texlive) scheme-medium latexmk koma-script babel-english
+            physics mathtools amsmath fontspec booktabs siunitx caption biblatex float
+            pgfplots microtype fancyvrb csquotes setspace newunicodechar hyperref
+            cleveref multirow bbold unicode-math biblatex-phys xpatch beamerposter
+            type1cm changepage lualatex-math footmisc wrapfig2 curve2e pict2e wrapfig
+            appendixnumberbeamer sidecap appendix orcidlink ncctools bigfoot crop xcolor;
+        };
+      in
+      rec {
+        packages = {
+          watch = pkgs.writeShellScriptBin "watch-latex" ''
+            ${tex}/bin/latexmk
+            while ${pkgs.inotify-tools}/bin/inotifywait --include ".*\.(sty|tex|pgf|pdf)" -e modify -r .; do
+              ${tex}/bin/latexmk
+            done
           '';
-          installPhase = ''
-            mkdir -p $out
-            cp output/index.pdf $out/
+          document = pkgs.stdenvNoCC.mkDerivation rec {
+            name = "masters-thesis";
+            src = self;
+            buildInputs = [ pkgs.coreutils tex pkgs.biber ];
+            phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+            buildPhase = ''
+              export PATH="${pkgs.lib.makeBinPath buildInputs}";
+              mkdir -p .cache/texmf-var
+              mkdir -p output/src
+              env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                 OSFONTDIR=${pkgs.gyre-fonts}/share/fonts:${pkgs.liberation_ttf}/share/fonts:${pkgs.lato}/share/fonts/lato:${pkgs.raleway}/share/fonts:${pkgs.lmodern}/share/fonts \
+                latexmk ./index.tex
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp output/index.pdf $out/
+            '';
+          };
+        };
+        defaultPackage = packages.document;
+        devShell = pkgs.mkShellNoCC {
+          buildInputs = [ packages.watch pkgs.openjdk ] ++ packages.document.buildInputs;
+          shellHook = ''
+            export OSFONTDIR=${pkgs.gyre-fonts}/share/fonts:${pkgs.liberation_ttf}/share/fonts:${pkgs.lato}/share/fonts/lato:${pkgs.raleway}/share/fonts:${pkgs.lmodern}/share/fonts
           '';
         };
-      };
-      defaultPackage = packages.document;
-      devShell = pkgs.mkShellNoCC {
-        buildInputs = [packages.watch pkgs.openjdk] ++ packages.document.buildInputs;
-        shellHook = ''
-          export OSFONTDIR=${pkgs.gyre-fonts}/share/fonts:${pkgs.liberation_ttf}/share/fonts:${pkgs.lato}/share/fonts/lato:${pkgs.raleway}/share/fonts:${pkgs.lmodern}/share/fonts
-        '';
-      };
-    });
+      });
 }
